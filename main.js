fc301acb1654593fcb824559126b37ef37ec5dd0
murilopereira555-tmu <murilo.pereira@torontomu.ca>
2024-10-07 22:47:39 -0400

    Added JS
    Added JavaScript Functionality and new pages for navigation

---
+++ main.js
@@ -0,0 +1,40 @@
+
+function name_prompt() {
+    var name = prompt("What is your name?"); //Prompt to get name
+    const user_nameOutput = document.getElementById("user_name"); //Find element id
+
+    user_nameOutput .textContent= "Welcome " + name + "!";
+
+}
+
+function enlarge() {
+
+    var profile_pic = document.getElementById("profile_pic"); //gets the profile pic
+
+    profile_pic.classList.toggle("enlarged"); //toggles the enlarged styling
+
+}
+
+function toggleDropdown() {
+    var dropdownContent = document.getElementById("dropdown-content");
+    
+    // Toggle between showing and hiding the dropdown
+    if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+    } else {
+        dropdownContent.style.display = "block";
+    }
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.style.display === "block") {
+                openDropdown.style.display = "none";
+            }
+        }
+    }
+}
